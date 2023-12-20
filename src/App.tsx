93be04e7126d81718dc216984cfbf4bcc7359563
kukla1989 <kukla.1989@ukr.net>
2023-12-20 21:54:23 +0200

    add pages and setup of routes

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
+import { Home, Store, About } from './pages/';
+function App() {
+  return (
+  <Container>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/store' element={<Store />} />
+    </Routes>
+  </Container>
+  )
+}
+
+export default App
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import { About } from './About';
+import { Store } from './Store';
+import { Home } from './Home';
+
+export { Home, About, Store };
